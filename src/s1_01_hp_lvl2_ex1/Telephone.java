18a0094aef03c731437759a0467c11a21c940ac4
resikrys <resicris@gmail.com>
2025-05-13 20:22:05 +0200

    add telephone class

---
+++ src/s1_01_hp_lvl2_ex1/Telephone.java
@@ -0,0 +1,28 @@
+package s1_01_hp_lvl2_ex1;
+
+public class Telephone {
+    private String brand;
+    private String model;
+
+    public Telephone(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+    public String getModel() {
+        return this.model;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void calling(String phoneNumber) {
+        System.out.println("Calling number " + phoneNumber);
+    }
+}
