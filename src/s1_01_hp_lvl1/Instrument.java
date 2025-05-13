664f5e4b25de87e8c82f318c1dbd833e7f63963c
resikrys <resicris@gmail.com>
2025-05-13 10:48:38 +0200

    add abstract instrument class

---
+++ src/s1_01_hp_lvl1/Instrument.java
@@ -0,0 +1,31 @@
+package s1_01_hp_lvl1;
+
+public abstract class Instrument {
+    private String name;
+    private float price;
+
+    public Instrument(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public abstract void tocar();
+
+    public String toString() {
+        return "Instrument named " + this.getName() + " costs: " + this.getPrice() + "€.";
+    }
+}
